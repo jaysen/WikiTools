24ba4b63516b88776ca9321f2abd526e66761b6f
Jaysen Naidoo <jaysen@gmail.com>
2021-03-23 10:17:37 +0200

    wiki classes, wikidpad wiki class + tests

---
+++ src/WikiLib/Wikis/LocalWiki.cs
@@ -0,0 +1,18 @@
+﻿using System.IO;
+
+namespace WikiLib.Wikis
+{
+
+    public abstract class LocalWiki : Wiki
+    {
+        // Constructor
+        protected LocalWiki(string rootPath)
+        {
+            if (!Directory.Exists(rootPath))
+            {
+                throw new DirectoryNotFoundException($"Directory {rootPath} does not exist");
+            }
+            Location = rootPath;
+        }
+    }
+}
---
+++ src/WikiLib/Wikis/WikidpadWiki.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using WikiLib.Pages;
+
+namespace WikiLib.Wikis
+{
+    public class WikidPadWiki : LocalWiki
+    {
+        public readonly string DataDir;
+        public WikidPadWiki(string rootPath) : base(rootPath)
+        {
+            DataDir = Path.Combine(rootPath, "data");
+            if (!Directory.Exists(DataDir))
+            {
+                DataDir = rootPath;
+            }
+
+            Pages = GetAllPages();
+        }
+
+
+        public sealed override List<Page> GetAllPages()
+        {
+            var files = Directory.EnumerateFiles(DataDir, "*.wiki");
+            var pages = new List<Page>();
+            foreach (var file in files)
+            {
+                pages.Add(new WikidpadPage(Path.Combine(DataDir, file)));
+            }
+
+            return pages;
+        }
+
+        public override List<Page> GetPagesBySearchStr()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
