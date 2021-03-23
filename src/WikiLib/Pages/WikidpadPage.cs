294b906e7424cc057cee943dc286412a0b0eaf33
Jaysen Naidoo <jaysen@gmail.com>
2021-03-23 10:16:07 +0200

    wikidpad page class

---
+++ src/WikiLib/Pages/WikidpadPage.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Collections.Generic;
+
+namespace WikiLib.Pages
+{
+    public class WikidpadPage : LocalPage
+    {
+        public WikidpadPage(string location) : base(location)
+        {
+            if (System.IO.Path.GetExtension(location) != ".wiki")
+            {
+                throw new FormatException("This is not a path to a .wiki page");
+            }
+        }
+
+        public override List<string> GetLinks()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public override List<string> GetAliases()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public override List<string> GetTags()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
