d8c7abdd91dfbfe033116bf2971f6ee8f9b87eff
Jaysen Naidoo <jaysen@gmail.com>
2021-03-23 10:10:48 +0200

    Solution with initial CLI project

---
+++ src/WikiTool.CLI/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace WikiTool.CLI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
